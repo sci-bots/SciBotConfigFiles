1f76ccc56cabc9f6b21f63905d0cdc05a9906457
lucaszw <lucas.zeer@gmail.com>
2017-07-13 11:06:09 -0400

    added install, and after_test scripts for appveyor

---
+++ appveyor-after-test.ps1
@@ -0,0 +1,26 @@
+# Get the package location, and package name
+$package_location = $(& cat PACKAGE_LOCATION).Trim();
+$package_name = (( $package_location -split '\\') | Select-Object -Last 1) -split '\.bz2' | Select-Object -First 1;
+
+# Unarchive package
+7z e $package_location -tbzip2;
+7z x $package_name -opackage -ttar;
+$json = Get-Content '.\\package\\info\\index.json' | Out-String | ConvertFrom-Json;
+
+# Add members to info.json:
+$json | Add-Member git_repository $($env:GIT_REPOSITORY);
+$json | ConvertTo-JSON | % { [System.Text.RegularExpressions.Regex]::Unescape($_) } | Set-Content .\package\info\index.json;
+
+# re-archive package
+7z a $package_name .\package\* -ttar;
+7z a $($package_name+'.bz2') $package_name -tbzip2;
+
+# Save tarfile as artifact
+appveyor PushArtifact $($package_name+'.bz2');
+
+# Upload to Anaconda Cloud
+# binstar -t $($BINSTAR_TOKEN) upload --force $($package_name+'.bz2');
+
+# Upload Test Results
+$wc = New-Object 'System.Net.WebClient';
+$wc.UploadFile("https://ci.appveyor.com/api/testresults/junit/$($env:APPVEYOR_JOB_ID)", (Resolve-Path .\nosetests.xml));
---
+++ appveyor-install.bat
@@ -0,0 +1,39 @@
+REM Batch file for AppVeyor install step
+REM Requires MINICONDA and PROJECT_NAME environment variables
+
+REM Add Conda to path
+set PATH=%MINICONDA%;%MINICONDA%\\Scripts;%PATH%
+
+REM Configure Conda to operate without user input
+conda config --set always_yes yes --set changeps1 no
+
+REM Add the conda-force, and wheeler-microfluidics channels
+conda config --add channels conda-forge
+conda config --add channels wheeler-microfluidics
+
+REM Update conda, and install conda-build (used for building in non-root env)
+conda update -q conda
+conda install --yes conda-build anaconda-client
+
+REM Create and activate new NadaMq environment
+conda create --name %PROJECT_NAME%
+call %MINICONDA%\\Scripts\\activate.bat %PROJECT_NAME%
+REM conda info -a
+
+REM Get output package location
+echo "Getting package location:"
+FOR /F "tokens=*" %%a in ('conda-build . --output') do SET PACKAGE_LOCATION=%%a
+echo %PACKAGE_LOCATION%
+
+REM Set environment variable for project location (may be used in bld.bat)
+set "PROJECT_DIRECTORY=%cd%"
+
+REM Build package
+conda-build .
+
+REM Move back to project directory
+cd %PROJECT_DIRECTORY%
+
+REM Capture package location
+touch PACKAGE_LOCATION
+echo %PACKAGE_LOCATION% > PACKAGE_LOCATION
